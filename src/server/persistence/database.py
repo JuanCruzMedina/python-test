4d9ad6cae6b0df6ab73b21dc84372975990e4ef0
Juan Cruz Medina <juancruzfaq@gmail.com>
2022-03-10 18:17:15 -0300

    Se modularizó la carga de usuarios y tareas.

--- src/server/persistence/database.py
+++ src/server/persistence/database.py
@@ -12,38 +12,23 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 # Establecemos una ruta estatica donde buscar los usuarios.
 USERS_PATH: str = "../../extra/data/users.json"
 TASKS_PATH: str = "../../extra/data/tasks.json"
+
+# Declaracion de constantes que almacenaran los valores de nuestra base de datos falsa.
 USERS: List[User] = []
 TASKS: List[Task] = []
 
 
-def load_users() -> List[User]:
-    lista_usuarios: List[User] = []
-    print('-- Cargando usuarios ...')
-    with io.open(USERS_PATH, 'r', encoding='utf-8') as jsonFile:
+def load(nombre: str, ruta: str, coleccion: List, clase) -> None:
+    print(f'-- Cargango {nombre} ...')
+    with io.open(ruta, 'r', encoding='utf8') as jsonFile:
         content = json.load(jsonFile)
-        for item in content:
-            user = User(**item)
-            lista_usuarios.append(user)
-
-    return lista_usuarios
-
-
-def load_tasks() -> List[Task]:
-    lista_tareas: List[Task] = []
-    print('-- Cargando usuarios ...')
-    with io.open(TASKS_PATH, 'r', encoding='utf-8') as jsonFile:
-        content = json.load(jsonFile)
-        for item in content:
-            task = Task(**item)
-            lista_tareas.append(task)
-
-    return lista_tareas
+        [coleccion.append(clase(**item)) for item in content]
 
 
 print(f'Usuarios antes de cargar:{len(USERS)}')
-USERS = load_users()
+load('usuarios', USERS_PATH, USERS, User)
 print(f'Usuarios cargados: {len(USERS)}')
 
 print(f'Usuarios antes de cargar:{len(TASKS)}')
-TASKS = load_tasks()
+load('tareas', TASKS_PATH, TASKS, Task)
 print(f'Usuarios cargados: {len(TASKS)}')
